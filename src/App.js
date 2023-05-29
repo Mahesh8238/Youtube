082d7bd5d1a5db14ee198ecb3857828baca63834
Mahesh8238 <maheshalloli006@gmail.com>
2023-05-29 17:45:37 +0530

    added navbar components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
 import './App.css';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
-     <h1>Youtube</h1>
+      <Navbar/>
     </div>
   );
 }
